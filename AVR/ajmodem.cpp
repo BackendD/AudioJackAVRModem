53054363a9364b3d1139980c5ecb1a66765d3c54
Hasan Karimi <destructor.iri@gmail.com>
2017-09-05 11:38:22 +0430

    Declare Modem::demodulate() method

--- AVR/ajmodem.cpp
+++ AVR/ajmodem.cpp
@@ -1,4 +1,5 @@
 #include "ajmodem.h"
+#include <avr/interrupt.h>
 
 Modem* Modem::activeObject = nullptr;
 
@@ -14,12 +15,25 @@ Modem::~Modem()
 	
 }
 
+enum { START_BIT = 0, DATA_BIT = 8, STOP_BIT = 9, INACTIVE = 0xff };
+	
 void Modem::setFreq(uint32_t freq)
 {
 	this->freq = freq;
 	baudRate = 1225;
 	lowFreq = 4900;
 	highFreq = 7350;
+	
+	low_freq_micros = (uint16_t) (1000000 / lowFreq);
+	micros_per_timer_count = (uint16_t) (8 / (freq/1000000));
+	tcnt_low_freq = low_freq_micros / micros_per_timer_count;
+	tcnt_low_th_l = tcnt_low_freq * 0.85;
+	tcnt_low_th_h = tcnt_low_freq * 1.10;
+	bit_period = (uint16_t) (1000000 / baudRate);
+	tcnt_bit_period = bit_period / micros_per_timer_count;
+	high_freq_micros = (uint16_t) (1000000/highFreq);
+	tcnt_high_freq = high_freq_micros / micros_per_timer_count;
+	tcnt_high_th_h = tcnt_high_freq * 1.15;
 }
 
 void Modem::begin(void)
@@ -34,7 +48,7 @@ void Modem::begin(void)
 	
 	Modem::activeObject = this;
 	
-	_lastTCNT = TCNT2;
+	_lastTCNT = TCNT1;
 	_lastDiff = _lowCount = _highCount = 0;
 	
 	TCCR1A &= ~((1<<WGM11) | (1<<WGM10));
@@ -42,4 +56,58 @@ void Modem::begin(void)
 	
 	ACSR &= ~(1<<ACIS0);
 	ACSR |= ((1<<ACIE) | (1<<ACIS1));
+}
+
+void Modem::demodulate(void)
+{
+	uint16_t t = TCNT1;
+	uint16_t diff;
+	
+	diff = t - _lastTCNT;
+	
+	if(diff < 4)
+		return;
+	
+	_lastTCNT = t;
+	
+	if(diff > tcnt_low_th_h)
+		return;
+	
+	_lastDiff = diff;
+
+	if(_lastDiff >= tcnt_low_th_l)
+	{
+		_lowCount += _lastDiff;
+		if(_recvStat == INACTIVE)
+		{
+			// Start bit detection
+			if(_lowCount >= tcnt_bit_period * 0.5)
+			{
+				_recvStat = START_BIT;
+				_highCount = 0;
+				_recvBits  = 0;
+				OCR1A = t + tcnt_bit_period - (uint16_t)_lowCount;
+				TIFR |= (1<<OCF1A);
+				TIMSK |= (1<<OCIE1A);
+			}
+		}
+	}
+	else if(_lastDiff <= (uint8_t)(tcnt_high_th_h))
+	{
+		if(_recvStat == INACTIVE)
+		{
+			_lowCount = 0;
+			_highCount = 0;
+		}
+		else
+		{
+			_highCount += _lastDiff;
+		}
+	}
+}
+
+// Analog comparator interrupt
+ISR(ANA_COMP_vect)
+{
+	Modem::activeObject->demodulate();
 }
--- AVR/ajmodem.h
+++ AVR/ajmodem.h
@@ -13,19 +13,33 @@ public:
 	void setFreq(uint32_t freq);
 	void begin(void);
 	static Modem* activeObject;
+	void demodulate(void);
 private:
 	uint32_t freq;
 	uint32_t baudRate;
 	uint32_t lowFreq;
 	uint32_t highFreq;
 	uint32_t rxBufSize;
+	uint16_t _lastTCNT;
 	uint8_t* _recvBuffer;
 	uint8_t _recvBufferHead;
 	uint8_t _recvBufferTail;
-	uint8_t _lastTCNT;
 	uint8_t _lastDiff;
+	uint8_t _recvStat;
+	uint8_t _recvBits;
 	uint8_t _lowCount;
 	uint8_t _highCount;
+		
+	uint16_t low_freq_micros;
+	uint16_t micros_per_timer_count;
+	uint16_t tcnt_low_freq;
+	uint16_t tcnt_low_th_h;
+	uint16_t tcnt_low_th_l;
+	uint16_t bit_period;
+	uint16_t tcnt_bit_period;
+	uint16_t high_freq_micros;
+	uint16_t tcnt_high_freq;
+	uint16_t tcnt_high_th_h;
 };
 
 #endif
