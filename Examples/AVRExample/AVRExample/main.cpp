32370abc89fa2480188acd6e43dbe224f4ded0ba
Hasan Karimi <destructor.iri@gmail.com>
2017-09-16 17:11:28 +0430

    Add lcd.h and use it.

--- Examples/AVRExample/AVRExample/main.cpp
+++ Examples/AVRExample/AVRExample/main.cpp
@@ -9,13 +9,25 @@
 
 #include <avr/io.h>
 #include <ajmodem.h>
+#include <util/delay.h>
+#include "lcd.h"
 
 int main(void)
 {
 	Modem modem = Modem(F_CPU, 32);
 	modem.begin();
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	lcd_puts("Ready");
+	
+
+	DDRD |= (1<<7);
     while (1) 
     {
+		_delay_ms(100);
+		PORTD &= ~(1<<7);
+		_delay_ms(100);
+		PORTD |= (1<<7);
     }
 }
 
